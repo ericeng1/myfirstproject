868c63daabef80f6cf46c454ae3171a2fc6b13a2
ericeng3000 <ericeng3000@gmail.com>
2026-02-10 22:21:14 -0500

    Add new feature files

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+console.log("script.js loaded");
+
 const supabaseUrl = "https://onlshmndstcyqdxpimtv.supabase.co";
-const supabaseKey = "sb_publishable_sKM_Z6dU7P9_033hGlDWpA_wRve0zt4";
+const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ubHNobW5kc3RjeXFkeHBpbXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MjQxNzAsImV4cCI6MjA4NDAwMDE3MH0.IV9gNNZnEwBepC2G1aDai-Kbu2zSLz06UkMyoSfJR0w";
 
-const supabase = window.supabase.createClient(supabaseUrl, supabaseKey);
+const supabaseClient = window.supabase.createClient(
+  supabaseUrl,
+  supabaseKey
+);
 
 
-// ✅ Test connection
+// Test connection
 async function testConnection() {
+
   console.log("Testing Supabase connection...");
 
-  const { data, error } = await supabase
+  const { data, error } = await supabaseClient
     .from("submissions")
     .select("*")
     .limit(1);
@@ -20,5 +26,4 @@ async function testConnection() {
   }
 }
 
-// Run test when page loads
 testConnection();
