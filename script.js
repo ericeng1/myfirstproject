1b6579293061bc66f36c846ce7d4dfcbffcebc86
ericeng3000 <ericeng3000@gmail.com>
2026-02-10 23:01:30 -0500

    Add new feature files

--- script.js
+++ script.js
@@ -2,6 +2,8 @@
 // Supabase Setup
 // ===============================
 
+console.log("script.js loaded");
+
 const supabaseUrl = "https://onlshmndstcyqdxpimtv.supabase.co";
 const supabaseKey = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im9ubHNobW5kc3RjeXFkeHBpbXR2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Njg0MjQxNzAsImV4cCI6MjA4NDAwMDE3MH0.IV9gNNZnEwBepC2G1aDai-Kbu2zSLz06UkMyoSfJR0w";
 
